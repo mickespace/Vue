5bc5f47bd2d255180caacfa48fd247572fc7c400
mickespace <micke_space@163.com>
2017-07-12 18:14:33 +0800

    company

--- views/common/layout.js
+++ views/common/layout.js
@@ -26,15 +26,13 @@ require.config({
 });
 
 require(['vue', 'jquery', 'bootstrap', 'bootsnav', 'iframe'], function (v, $) {
-    $(function () {
-        startInit('mainFrame');
-    });
+
     var layout = new v({
         el: '#layout',
         data: {
+            isbusy: false,
             operModel: {
                 isLoginMode: true,
-                Name: '登录界面'
             },
             userInfo: {
                 Name: '陈俊良',
@@ -54,11 +52,6 @@ require(['vue', 'jquery', 'bootstrap', 'bootsnav', 'iframe'], function (v, $) {
         methods: {
             SwitchMode: function () {
                 layout.operModel.isLoginMode = !layout.operModel.isLoginMode;
-                if (layout.operModel.isLoginMode) {
-                    Layout.operModel.Name = "登录界面";
-                } else {
-                    Layout.operModel.Name = "注册界面";
-                }
             },
 
             Login: function () {
@@ -70,10 +63,21 @@ require(['vue', 'jquery', 'bootstrap', 'bootsnav', 'iframe'], function (v, $) {
             },
             Register: function () {
 
+            },
+
+            NavigationTo: function (routerUrl, data, height) {
+                var iframeurl = "../" + routerUrl + ".html?data=" + data;
+                var bsiframe = $("#mainFrame");
+                bsiframe.attr("height", height + 'px');
+                bsiframe.attr("src", iframeurl);
             }
         }
     });
-
+    $(function () {
+        var bsiframe = $("#mainFrame");
+        bsiframe.attr("height", '2950px');
+        bsiframe.attr("src", '../index/index.html');
+    })
     $(function () {
         $("#send_code").click(function () {
             $(this).button('loading').delay(1000).queue(function () {
@@ -81,4 +85,6 @@ require(['vue', 'jquery', 'bootstrap', 'bootsnav', 'iframe'], function (v, $) {
             });
         });
     });
+
+
 });
